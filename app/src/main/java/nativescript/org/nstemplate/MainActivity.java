c85d4f4167d55f5c9838177004bea786a3893be5
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2017-10-19 11:34:43 +0300

    as 2.3.3 empty project

---
+++ app/src/main/java/nativescript/org/nstemplate/MainActivity.java
@@ -0,0 +1,12 @@
+package nativescript.org.nstemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
