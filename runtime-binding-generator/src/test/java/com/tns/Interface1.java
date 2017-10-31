7333ad982067f455d36066db8622d19d5aec4896
plamen5kov <plamen.krasimirov.petkov@gmail.com>
2017-10-31 14:41:59 +0200

    normal app with no runtime debugging

--- runtime-binding-generator/src/test/java/com/tns/Interface1.java
+++
@@ -1,9 +0,0 @@
-package com.tns;
-
-/**
- * Created by plamen5kov on 9/21/16.
- */
-
-public interface Interface1 {
-    void methodFromInterface();
-}
--- runtime/src/main/java/com/tns/FileExtractor.java
+++
@@ -1,5 +0,0 @@
-package com.tns;
-
-public interface FileExtractor {
-    boolean extract(android.content.Context context);
-}
